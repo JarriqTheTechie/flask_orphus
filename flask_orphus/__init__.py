3429e0726dbc378577a54c39954928b30176ee1e
jrolle <jrolle@equitybahamas.com>
2023-12-10 21:11:36 -0500

    Added stubs and a new command "new-project" to setup project directories.

--- flask_orphus/__init__.py
+++ flask_orphus/__init__.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import shutil
 from typing import Literal
 
 import click
@@ -14,6 +15,7 @@ from flask_orphus.routing.content_collection import ContentNotFoundError
 from flask_orphus.routing.fs_router import FSRouter
 from flask_orphus.routing.fs_router_api import FSRouterAPI
 from flask_orphus.validation import ValidationError
+from dotenv import load_dotenv
 
 routing_modes = Literal[
     "pages",
@@ -21,6 +23,7 @@ routing_modes = Literal[
     "hybrid"
 ]
 
+load_dotenv()
 
 
 class FlaskOrphus:
@@ -34,6 +37,7 @@ class FlaskOrphus:
             app.add_template_global(Request.session().old, name="old")
             app.add_template_global(Request.session().errors, name="errors")
             app.add_template_global(String, name="String")
+            app.add_template_global(os.getenv, name="env")
             match routing_mode:
                 case "hybrid":
                     FSRouter(app)
@@ -42,7 +46,6 @@ class FlaskOrphus:
                     FSRouterAPI(app)
                 case _:
                     FSRouter(app)
-
 
             self.init_app(self.app)
 
@@ -78,6 +81,51 @@ class FlaskOrphus:
         def contentNotFoundError(e):
             raise e
 
+        @app.cli.command("new-project")
+        def new_project():
+            import inspect
+            stubs_dir = os.path.join(os.path.dirname(inspect.getfile(inspect.currentframe())), "stubs")
+            dirs_to_create = [
+                "components", "layouts", "pages", "databases/migrations", "databases/seeds",
+                "lib/events", "lib/exceptions", "lib/jobs", "lib/mail", "lib/models",
+                "lib/notifications", "lib/observers", "lib/repositories", "lib/services",
+                "pages/auth"
+            ]
+            for folder in dirs_to_create:
+                try:
+                    print(f"Creating Directory {folder}")
+                    os.makedirs(f"{folder}")
+                except FileExistsError:
+                    print(f"Directory {folder} already exists. ")
+
+            try:
+                print(f"Creating File requirements.txt")
+                with open("requirements.txt", "x") as f:
+                    requirements = open(stubs_dir + "/requirements.txt", "r")
+                    f.write(
+                        requirements.read()
+                    )
+            except FileExistsError:
+                print(f"File Already Exisits - requirements.txt")
+
+            print(f"Creating File .env")
+            try:
+                with open(".env", "x") as f:
+                    env = open(stubs_dir + "/.env", "r")
+                    f.write(env.read())
+                print("Successfully created new Orphus Project.")
+            except FileExistsError:
+                print(f"File Already Exisits - .env")
+
+            print(f"Creating File pages/auth/logout.py")
+            try:
+                with open("pages/auth/logout.py", "x") as f:
+                    logout = open(stubs_dir + "/logout.py", "r")
+                    f.write(logout.read())
+                print("Successfully created new Orphus Project.")
+            except FileExistsError:
+                print(f"File Already Exisits - pages/auth/logout.py")
+
         @app.cli.command("make:model")
         @click.argument("model")
         def create_model(model):
@@ -101,4 +149,3 @@ class FlaskOrphus:
                 "--create", f"{table_name}",
             ], capture_output=True, text=True)
             print(result.stdout)
-
